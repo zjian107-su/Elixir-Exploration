cecbfac078e239d993b2508a620b3c2f0538e475
Zezheng Jiang <Zjian107@syr.edu>
2022-10-07 21:03:05 -0400

    ✨update: doctest and test with ....
    Finished in 0.03 seconds (0.00s async, 0.03s sync)
    2 doctests, 2 tests, 0 failures
    
    Randomized with seed 337839

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,6 +2,15 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 12
+  end
+
+  test "shuffling a deck randomize it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
   # Comment: Singular assertion test
   # test "The Trueth" do
   #   assert 1 + 1 == 3
