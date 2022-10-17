efe7e59d0a0d3cdf1eb24cb3cb8aea9e726f517c
Zezheng Jiang <Zjian107@syr.edu>
2022-10-16 20:46:57 -0400

    ✨update: add building_map function

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,6 +4,32 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_sqaures
+    |> build_pixel_map
+  end
+
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def filter_odd_sqaures(%Identicon.Image{grid: grid} = image) do
+    Enum.filter(grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end)
+
+    %Identicon.Image{image | grid: grid}
+
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -13,5 +13,5 @@ defmodule Identicon.Image do
       iex> %Identicon.Image{hex: []}
       %Identicon.Image{hex: []}
   """
-  defstruct hex: nil, color: nil, grid: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
